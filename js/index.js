46be6267dc583321e83e8b2815e60f9259f800ea
Pab Her <peh_tj@hotmail.com>
2022-10-11 17:03:04 -0300

    Primera Subida al Repositorio

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let whatsappOpenClose = document.getElementById('wahtsapp_pop')
+let whatsappChat = document.querySelector('.wahtsapp_chat_container')
+let chat_1 = document.getElementById('chat_1')
+let chat_2 = document.getElementById('chat_2')
+let chat_dots = document.getElementById('chat_dots')
+let chat_hs = document.querySelectorAll('.chat_hs')
+
+whatsappOpenClose.addEventListener('click', () => {
+    whatsappChat.classList.toggle('show')
+
+    let momentoActual = new Date()
+
+    for( let hs of chat_hs){
+        hs.innerText = momentoActual.getHours() + ":" + momentoActual.getMinutes()
+    }
+
+    setTimeout(() => {
+        chat_dots.classList.add('hidden')
+        chat_1.classList.add('visible')
+    },4000)
+    setTimeout(() => {
+        chat_2.classList.add('visible')
+    },6000)    
+})
