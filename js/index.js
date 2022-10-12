edcd9582d69ac0b428080418a836bec5d49e1ccd
Pab Her <peh_tj@hotmail.com>
2022-10-12 09:08:59 -0300

    Animacion Pulso y Minutos

--- js/index.js
+++ js/index.js
@@ -1,24 +1,44 @@
-let whatsappOpenClose = document.getElementById('wahtsapp_pop')
-let whatsappChat = document.querySelector('.wahtsapp_chat_container')
-let chat_1 = document.getElementById('chat_1')
-let chat_2 = document.getElementById('chat_2')
-let chat_dots = document.getElementById('chat_dots')
-let chat_hs = document.querySelectorAll('.chat_hs')
+let pulso = document.querySelector('.pulso');
+
+pulso.addEventListener('animationend', () => {
+    pulso.classList.remove('animate');
+});
+
+let ejecutarPulso = () => {
+    setInterval(() => {
+        pulso.classList.add('animate')
+    },7000);
+};
+
+window.addEventListener('load',() =>{
+    ejecutarPulso()
+});
+
+let whatsappOpenClose = document.getElementById('wahtsapp_pop');
+let whatsappChat = document.querySelector('.wahtsapp_chat_container');
+let chat_1 = document.getElementById('chat_1');
+let chat_2 = document.getElementById('chat_2');
+let chat_dots = document.getElementById('chat_dots');
+let chat_hs = document.querySelectorAll('.chat_hs');
 
 whatsappOpenClose.addEventListener('click', () => {
-    whatsappChat.classList.toggle('show')
+    whatsappChat.classList.toggle('show');
 
-    let momentoActual = new Date()
+    let momentoActual = new Date();
+
+    let minutosActual = momentoActual.getMinutes() < 10 ? 
+    0 + momentoActual.getMinutes() : momentoActual.getMinutes();
 
     for( let hs of chat_hs){
-        hs.innerText = momentoActual.getHours() + ":" + momentoActual.getMinutes()
-    }
+        hs.innerText = momentoActual.getHours() + ":" + minutosActual
+    };
 
     setTimeout(() => {
-        chat_dots.classList.add('hidden')
-        chat_1.classList.add('visible')
-    },4000)
+        chat_dots.classList.add('hidden');
+        chat_1.classList.add('visible');
+    },4000);
     setTimeout(() => {
-        chat_2.classList.add('visible')
-    },6000)    
-})
+        chat_2.classList.add('visible');
+    },6000)    ;
+});
+
